af3c22488729e6dc93901b548c628b87b3e9e847
Winnie <winnieng3210@gmail.com>
2021-08-30 15:07:17 -0400

    make pokemon smaller

--- skeleton-part-ii/app/assets/javascripts/bundle.js
+++ skeleton-part-ii/app/assets/javascripts/bundle.js
@@ -271,8 +271,11 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react_router_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-router-dom */ "./node_modules/react-router-dom/esm/react-router-dom.js");
 
 
+
 var PokemonIndexItem = function PokemonIndexItem(props) {
-  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", null, props.pokemon.name, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", {
+    className: "pokemon-index-item"
+  }, props.pokemon.name, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
     src: props.pokemon.imageUrl
   }));
 };
--- skeleton-part-ii/frontend/components/pokemon/pokemon_index_item.jsx
+++ skeleton-part-ii/frontend/components/pokemon/pokemon_index_item.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export const PokemonIndexItem = (props) => {
     return (
-        <li>
+        <li className = "pokemon-index-item">
             {props.pokemon.name}
             <img src= {props.pokemon.imageUrl} />
         </li>
