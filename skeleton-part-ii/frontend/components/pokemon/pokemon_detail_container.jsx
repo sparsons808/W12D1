823f32aed5e4b5354bf6edc74d14caa059099672
Winnie <winnieng3210@gmail.com>
2021-08-30 17:22:48 -0400

    end of day

---
+++ skeleton-part-ii/frontend/components/pokemon/pokemon_detail_container.jsx
@@ -0,0 +1,12 @@
+// import React, { Component } from 'react'
+import PokemonDetail from "./pokemon_detail";
+
+const mapStateToProps = (state) => ({
+    
+})
+
+const mapDispatchToProps = {
+    
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PokemonDetail)
